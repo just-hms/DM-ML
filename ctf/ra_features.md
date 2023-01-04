f145ae846c1c7d4d9215bf5fafca9dbdce03a220
just-Hms <alessandroversari1@gmail.com>
2023-01-04 15:45:44 +0100

    found correct features to extract

---
+++ ctf/ra_features.md
@@ -0,0 +1,37 @@
+https://manpages.ubuntu.com/manpages/bionic/man1/ra.1.html
+
+saddr       
+sport			 
+daddr       	 
+dport			 
+proto			 
+state			 
+dur				 
+sbytes			 
+dbytes			 
+sttl			 
+dttl			 
+sloss			 
+dloss			 
+dport			was "service"
+sload			 
+dload			 
+spkts			 
+dpkts			 
+swin			 
+dwin			 
+stcpb			 
+dtcpb			 
+smeansz			 
+dmeansz			 
+trans		
+res_bdy_len		not_used	 
+sjit			 
+djit			 
+stime			 
+ltime			 
+sintpkt			 
+dintpkt			 
+tcprtt			 
+synack			 
+ackdat			 
---
+++ ctf/script.sh
@@ -0,0 +1,41 @@
+argus -r out_2.pcap -w filename.argus
+ra -r filename.argus -c ',' -u -s 
+
+srcid, stime, ltime, sstime, dstime, sltime, dltime, trans, seq, flgs, dur, avgdur, stddev, mindur, maxdur, saddr, daddr, proto, sport, dport, stos, dtos, sdsb, ddsb sco, dco, sttl, dttl, sipid, dipid, smpls, dmpls, svlan, dvlan svid, dvid, svpri, dvpri, spkts, dpkts, sbytes, dbytes, sappbytes, dappbytes, sload, dload, sloss, dloss, sploss, dploss, srate, drate, smac, dmac, dir, sintpkt, dintpkt, sjit, djit, state, suser, duser, swin, dwin, trans, srng, erng, stcpb, dtcpb, tcprtt, inode, offset, smaxsz, dmaxsz, sminsz, dminsz
+
+
+srcip
+sport
+dstip
+dsport
+proto
+state
+dur
+sbytes
+dbytes
+sttl
+dttl
+sloss
+dloss
+service
+Sload
+Dload
+Spkts
+Dpkts
+swin
+dwin
+stcpb
+dtcpb
+smeansz
+dmeansz
+trans_depth
+res_bdy_len
+Sjit
+Djit
+Stime
+Ltime
+Sintpkt
+Dintpkt
+tcprtt
+synack
+ackdat
