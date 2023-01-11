d4b15838492a5ccb3abb41a26a9d52f4b3cf7eb3
just-Hms <alessandroversari1@gmail.com>
2023-01-11 20:40:15 +0100

    chooseMostAccurateClassificator

--- src/lib/classification.py
+++ src/lib/classification.py
@@ -1,105 +1,84 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
 from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, RocCurveDisplay
-from pandas import read_csv
+
+import pandas as pd
 from numpy import ravel
 import matplotlib.pyplot as plt
 
 from principal_component_analysis import PCA_extract
 
+def chooseMostAccurateClassificator(X, labels, classifiers):
+
+	best_model = {}
+	best_score = 0
+
+	training_data, testing_data, training_labels, testing_labels = train_test_split(
+		X,
+		labels,
+	)
+
+	isMulticlass = len(pd.value_counts(labels)) > 2 
+
+	#For each classifier, a model is created and tested
+	for k, v in classifiers.items():
+		
+		model = v.fit(
+			training_data,
+			ravel(training_labels)
+		)
+
+		predicted_labels = model.predict(testing_data)
+
+		score = accuracy_score(ravel(testing_labels), predicted_labels)
+
+		print("  ", k, score)
+
+		if score > best_score:
+			best_model = model
+			best_score = score
+
+		ConfusionMatrixDisplay.from_predictions(ravel(testing_labels), predicted_labels, normalize = "true")
+		plt.show()
+
+		if not isMulticlass:
+			RocCurveDisplay.from_predictions(ravel(testing_labels), predicted_labels)
+			plt.show()
+
+		return best_model, best_score
+
 # Extracting data
 
-data = read_csv('./preprocessing/UNSW-NB15-preprocessed.csv',low_memory=False)
-
+data = pd.read_csv('./../assets/UNSW-NB15-preprocessed.csv',low_memory=False)
 data.info()
 
 X, labels, classes = data.iloc[:, 1:30], data.iloc[:, 32], data.iloc[:, 31]
-# X, labels, classes, components = PCA_extract(data, verbose=True)
 
-# Binary classification
-
-#Creation of the training set
-
-training_data, testing_data, training_labels, testing_labels = train_test_split(
-	X,
-	labels,
-)
-
-print("Binary classifier: ")
-
-#Multiple types of classifier are tested
-classifiers = {
+classifiers={
 	"randomforest" : RandomForestClassifier(),
 }
 
-#For each classifier, a model is created and tested
-for k, v in classifiers.items():
-	
-	model = v.fit(
-		training_data,
-		ravel(training_labels)
-	)
+print("Binary classifier")
 
-	predicted_labels = model.predict(testing_data)
+chooseMostAccurateClassificator(X, labels, classifiers=classifiers)
 
-	score = accuracy_score(ravel(testing_labels), predicted_labels)
-
-	print(k, score)
-
-	print("Confusion matrix")
-	#disp = ConfusionMatrixDisplay(confusion_matrix(ravel(testing_labels), predicted_labels, normalize = "all"))
-	ConfusionMatrixDisplay.from_predictions(ravel(testing_labels), predicted_labels, normalize = "true")
-	plt.show()
-
-	print("ROC curve")
-	RocCurveDisplay.from_predictions(ravel(testing_labels), predicted_labels)
-	plt.show()
-	
- 
-# Multiclass classification
-
-print("Multiclass classifier")
-
-# Creation of the training set
+# removing non attacks
 X = X[labels != 0]
 classes = classes[labels != 0]
 
-training_data, testing_data, training_labels, testing_labels = train_test_split(
-	X, 
-	classes,
-)
+print("Multiclass classifier")
+chooseMostAccurateClassificator(X, classes, classifiers=classifiers)
 
-#Multiple types of classifier are tested
+# PCA
 
-classifiers = {
-	"randomforest" : RandomForestClassifier(),
-}
+X, labels, classes, components = PCA_extract(data, verbose=True)	
 
-for k, v in classifiers.items():
-	model = v.fit(
-		training_data,
-		ravel(training_labels)
-	)
+print("Binary classifier")
+chooseMostAccurateClassificator(X, labels, classifiers=classifiers)
 
-	predicted_labels = model.predict(testing_data)
-	score = accuracy_score(ravel(testing_labels), predicted_labels)
+# removing non attacks
+X = X[labels != 0]
+classes = classes[labels != 0]
 
-	print(k, score)
-
-	print("Confusion matrix:")
-	#disp = ConfusionMatrixDisplay(confusion_matrix(ravel(testing_labels), predicted_labels, normalize = "all"), display_labels = ravel(testing_labels))
-	ConfusionMatrixDisplay.from_predictions(ravel(testing_labels), predicted_labels, normalize = "true")
-	plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
+print("Multiclass classifier")
+chooseMostAccurateClassificator(X, classes, classifiers=classifiers)
