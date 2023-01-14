ab694bb1e15e562c2b7974524a56f9fb2a030f23
just-Hms <alessandroversari1@gmail.com>
2023-01-14 18:37:27 +0100

    boxplot added to pca

--- src/classification/principal_component_analysis.py
+++ src/classification/principal_component_analysis.py
@@ -1,13 +1,14 @@
+from matplotlib.pyplot import boxplot
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-def PCA_extract(X):
+def PCA_extract(data):
 	
 	pca = PCA(n_components=0.90, svd_solver='full')
 	sc = StandardScaler()
 
-	X = sc.fit_transform(X)
+	X = sc.fit_transform(data)
 	X = pca.fit_transform(X)
 
 	table = pd.DataFrame(
@@ -16,5 +17,14 @@ def PCA_extract(X):
 	)
 
 	print(table * 100)
+
+	comps = pd.DataFrame(pca.components_, columns = data.columns)
+
+	# print(comps)
+
+	print(comps.apply(lambda x: abs(x)).mean().sort_values(ascending = False))
+
+	boxplot(comps.apply(lambda x: abs(x)))
+
 
 	return X, pca, sc
